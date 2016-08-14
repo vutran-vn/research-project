3ef0303c31c02b43a134018870edbeef69a897a6
Yi-Chan Tsai <Yi-Chan.tsai@studnet.uts.edu.au>
2016-08-14 17:09:55 +1000

    [python] [first layer] Add detection of any updates in new categories. (330 categories. 14 Aug 2016)
    Add ',' delimiter in a text file.

--- python/parse.py
+++ python/parse.py
@@ -18,7 +18,10 @@ url_category = url_main + "/category"
 
 r = requests.get(url_browse)
 soup = BeautifulSoup(r.content, "html.parser")
-# First layer: list category urls, names and counting numbers
+##########################################################################################################################################
+# First layer:  http://www.serchen.co.uk/browse/
+# Purpose:  List all category urls, names and counting numbers
+##########################################################################################################################################
 links = soup.find_all("li")
 for link in links:
     try:
@@ -27,13 +30,13 @@ for link in links:
             category_url = url_main + str(link.contents[0].get('href'))
             category_name = str(link.contents[0].text)
             count_number = str(link.contents[1].text)
-            print (category_url, category_name, count_number)
+            print (category_url + ',' + category_name + ',' +count_number)
     except:
         pass
 sys.stdout = original
 f.close()
-# Detection of any updates in new categories
-if len(glob.glob('./*[0-9]*.txt')) == 2:
+# Detection of any updates in new categories. (330 categories. 14 Aug 2016)
+if len(glob.glob('*[0-9]*.txt')) == 2:
     file_list = glob.glob('*[0-9]*.txt')
 
     print ('Comparing ' + file_list[0] + ' and ' + file_list[1] + ' ...')
@@ -41,10 +44,42 @@ if len(glob.glob('./*[0-9]*.txt')) == 2:
     if filecmp.cmp(file_list[0], file_list[1]):
         print ('All the same. Exit!')
     else:
-        print('Found difference! Updating...')
+        file_old = open(file_list[0])
+        file_new = open(file_list[1])
 
+        file_old_line = file_old.readline().rstrip('\n')
+        file_new_line = file_new.readline().rstrip('\n')
+        line_no = 1
+        print('Found difference! Updating ...')
+        # Loop if either file_old or file_new has not reached EOF
+        while file_old_line != '' or file_new_line != '':
+                # Compare the lines from both file
+                if file_old_line != file_new_line:
+                    # If a line does not exist on file_new then mark the output with + sign
+                    if file_new_line != '' and file_new_line == '':
+                        print (">+", "Line-%d" % line_no, file_old_line)
+                    # otherwise output the line on file_old and mark it with > sign
+                    elif file_old_line != '':
+                        print(">", "Line-%d" % line_no, file_old_line)
+                    # If a line does not exist on file_old then mark the output with + sign
+                    if file_old_line == '' and file_new_line != '':
+                        print (">+", "Line-%d" % line_no, file_new_line)
+                    # otherwise output the line on file_old and mark it with > sign
+                    elif file_new_line != '':
+                        print(">", "Line-%d" % line_no, file_new_line)
+                file_old_line = file_old.readline().rstrip('\n')
+                file_new_line = file_new.readline().rstrip('\n')
+                line_no += 1
+        file_old.close()
+        file_new.close()
     print ('Deleting ' + file_list[0] + '...')
     os.remove(file_list[0])
 
-# Second layer: List this category name, service urls, names, listed date, star and the number of reviews
-# Third layer: list the category name, the service url on serchen.co.ul, the service official site, stars, detailed reviews, and the date of comments
+##########################################################################################################################################
+# Second layer: e.g. http://www.serchen.co.uk/category/appointments-scheduling/
+# Purpose:  List this category name, service urls, names, listed date, star and the number of reviews
+##########################################################################################################################################
+##########################################################################################################################################
+# Third layer:  e.g. http://www.serchen.co.uk/company/3sixtylite/
+# Purpose:  List the category name, the service url on serchen.co.uk, url, the service official site, stars, detailed reviews, and the date of comments
+##########################################################################################################################################
